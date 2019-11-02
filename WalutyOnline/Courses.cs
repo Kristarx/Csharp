ecb24b891eda99c00ce1c3830cd24c538df8877e
Kristarx <46490089+Kristarx@users.noreply.github.com>
2019-11-03 00:08:42 +0100

    Add files via upload

---
+++ WalutyOnline/Courses.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Net;
+
+namespace WalutyOnline
+{
+    public partial class Courses : Form
+    {
+        public Courses()
+        {
+            InitializeComponent();
+            dataGridView1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            dataGridView1.Visible = true;
+            textBox1.Visible = true;
+            label2.Visible = true;
+            label1.Visible = false;
+            pictureBox1.Width = 457;
+            pictureBox1.Height = 477;
+            pictureBox1.Location = new Point(496, 12);
+            label3.Visible = true;
+            dateTimePicker1.Visible = true;
+            dateTimePicker1.MaxDate = DateTime.Now;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            string date = dateTimePicker1.Value.ToString("yyMMdd");
+            DataInGrid(date);   
+        }
+
+        private void DataInGrid(string date)
+        {
+            XmlDocument currency = new XmlDocument();
+            string currOfClient = textBox1.Text.ToString().ToUpper();
+            try
+            {
+                currency.Load(@"Kursy\" + date + ".xml");
+                XmlNodeList types_of_curr = currency.GetElementsByTagName("pozycja");
+                foreach(XmlNode data in types_of_curr)
+                {
+                    if(textBox1.Text.ToString().Equals(""))
+                    {
+                        dataGridView1.Rows.Add(data.FirstChild.InnerText, data.FirstChild.NextSibling.InnerText, data.LastChild.PreviousSibling.InnerText, data.LastChild.InnerText);
+                    }
+                    else if(data.LastChild.PreviousSibling.InnerText.Contains(currOfClient))
+                    {
+                        dataGridView1.Rows.Add(data.FirstChild.InnerText, data.FirstChild.NextSibling.InnerText, data.LastChild.PreviousSibling.InnerText, data.LastChild.InnerText);
+                    }
+                    
+                }
+            }
+            catch
+            {
+                MessageBox.Show("There's no data for today or data are crashed");
+            }
+        }
+    }
+}
---
+++ WalutyOnline/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace WalutyOnline
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Visible = true;
+            progressBar1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            label2.Visible = true;
+            progressBar1.Visible = true;
+
+            GetData GT = new GetData();
+            int counter = 1;
+
+            if (GT.CheckConnection())
+            {
+                int DaysDiff = GT.ReturnDiffDays();
+                int div = DaysDiff / 20;
+
+                for (int i = 0; i <= DaysDiff; i++)
+                {
+                    counter = GT.DownloadFromInternet(i, counter);
+                    if(i%div == 0)
+                    {
+                        progressBar1.Increment(5);
+                    }
+                }
+
+                progressBar1.Value = 100;
+                this.Hide();
+                Courses cs = new Courses();
+                cs.Show();
+            }
+            else
+            {
+                MessageBox.Show("There's no internet connection. Data will be downloaded from files saved on your computer");
+            }
+        }
+    }
+}
